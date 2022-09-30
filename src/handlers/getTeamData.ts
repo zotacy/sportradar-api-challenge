af2721be4aaa574ca0c998bdb837a0adb10f64f7
Zach Tacy <zachtacy@gamil.com>
2022-09-29 21:31:51 -0500

    initial commit & set-up

---
+++ src/handlers/getTeamData.ts
@@ -0,0 +1,9 @@
+// async function getTeam(id: number, season: string) {
+//     let response = await fetch("https://statsapi.web.nhl.com/api/v1/teams");
+//     let teams = await response.json
+//     return teams;
+// }
+
+// getTeamData().then(response =>{
+//     console.log(response)
+// })
